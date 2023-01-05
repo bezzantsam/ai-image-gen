3b9c3e77a5af0639b7d32df21ea36cfb02ffb8a6
Sam <bezzantsam@gmail.com>
2023-01-05 09:25:10 -0800

    server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
